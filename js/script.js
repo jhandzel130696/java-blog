79c34af5cf91db2e4ce2b658dc27997971850ec2
jhandzel130696 <jhandzel130696@gmail.com>
2021-03-28 19:33:52 +0200

    Final sidebar

--- js/script.js
+++ js/script.js
@@ -31,8 +31,12 @@ for(let activeArticle of activeArticles){
 
   
     /* find the correct article using the selector (value of 'href' attribute) */
- 
+   const targetArticle = document.querySelector(articleSelector);
+   console.log (targetArticle);
     /* add class 'active' to the correct article */
+
+    
+    targetArticle.classList.add('active');
   }
   
   const links = document.querySelectorAll('.titles a');
