eccb884abaaec40d5857c44e71777e7829b6d985
jhandzel130696 <jhandzel130696@gmail.com>
2021-03-30 19:15:32 +0200

    finish functiongenerateTitlelinks

--- js/script.js
+++ js/script.js
@@ -39,11 +39,7 @@ for(let activeArticle of activeArticles){
     targetArticle.classList.add('active');
   }
   
-  const links = document.querySelectorAll('.titles a');
-  
-  for(let link of links){
-    link.addEventListener('click', titleClickHandler);
-  }
+ 
 
 
   const optArticleSelector = '.post',
@@ -55,19 +51,48 @@ for(let activeArticle of activeArticles){
     /* remove contents of titleList */
   const titleList = document.querySelector(optTitleListSelector);
    titleList.innerHTML = '';
+
     /* for each article */
   const articles = document.querySelectorAll(optArticleSelector);
+  let html='';
   for (let article of articles) {
+    
       /* get the article id */
   const articleId=article.getAttribute("id");
+
       /* find the title element */
+  const articleTitle = article.querySelector(optTitleSelector).innerHTML
   
+
       /* get the title from the title element */
-  
+     
+   
       /* create HTML of the link */
-  
+      const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+      console.log(linkHTML);
       /* insert link into titleList */
-  }
-  }
+      html=html +linkHTML;
+      console.log(html);
+
+  }   
+titleList.innerHTML = html;
+
+  
+
+/*insert listener to end*/ 
+  
+const links = document.querySelectorAll('.titles a');
+console.log(links);
+
+for(let link of links){
+  link.addEventListener('click', titleClickHandler);}
+
+
+
+
+
+
+
+}
   
   generateTitleLinks();
