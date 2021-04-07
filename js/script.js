8c244c9a8794fbda76b448a9690ababe528b3e0d
jhandzel130696 <jhandzel130696@gmail.com>
2021-04-07 20:51:36 +0200

    zmienienie tag na document w stalej alllinkstag

--- js/script.js
+++ js/script.js
@@ -157,7 +157,7 @@ TagLink.classList.remove('active');
   /* END LOOP: for each active tag link */
 }
   /* find all tag links with "href" attribute equal to the "href" constant */
-const allLinksTag = tag.querySelectorAll('a[href="' + href + '"]');
+const allLinksTag = document.querySelectorAll('a[href="' + href + '"]');
   /* START LOOP: for each found tag link */
 for (let TagLink of allLinksTag) {
     /* add class active */
